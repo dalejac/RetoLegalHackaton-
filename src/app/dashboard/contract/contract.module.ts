84d0780b530769ac7a8e46bba98ce3982a7a6312
dalejac <k-da929@hotmail.com>
2020-11-26 00:14:24 -0500

    creacion componente de download

--- src/app/dashboard/contract/contract.module.ts
+++ src/app/dashboard/contract/contract.module.ts
@@ -5,10 +5,11 @@ import { ContractRoutingModule } from './contract-routing.module';
 import { CreateContractComponent } from './create-contract/create-contract.component';
 import { ContractStatesComponent } from './contract-states/contract-states.component';
 import {SharedModule} from '../../shared/shared.module';
+import { DownloadContractComponent } from './download-contract/download-contract.component';
 
 
 @NgModule({
-  declarations: [CreateContractComponent, ContractStatesComponent],
+  declarations: [CreateContractComponent, ContractStatesComponent, DownloadContractComponent],
   imports: [
     CommonModule,
     ContractRoutingModule,
