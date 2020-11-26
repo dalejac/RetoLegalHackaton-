7837d2f51a9e855de4391d79a20d3afaa0fafd6f
dalejac <k-da929@hotmail.com>
2020-11-25 22:24:41 -0500

    agregando docx

--- src/app/dashboard/contract/contract-states/contract-states.component.ts
+++ src/app/dashboard/contract/contract-states/contract-states.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Packer } from 'docx';
+import { saveAs } from 'file-saver';
+
+import { buildContract } from './contract-data';
+import { DocumentCreator } from './contract-generator';
 
 @Component({
   selector: 'app-contract-states',
   templateUrl: './contract-states.component.html',
   styleUrls: ['./contract-states.component.scss']
 })
-export class ContractStatesComponent implements OnInit {
+export class ContractStatesComponent {
 
-  constructor() { }
+  download(): void{
+    const documentCreator = new DocumentCreator();
+    const doc = documentCreator.create([
+      buildContract,
+    ]);
 
-  ngOnInit(): void {
+    Packer.toBlob(doc).then(blob => {
+      saveAs(blob, 'contract.docx');
+    });
   }
+
 
 }
