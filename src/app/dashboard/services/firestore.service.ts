7ddbf515f3409cecc21df8b063837fff5822aac5
dalejac <k-da929@hotmail.com>
2020-11-26 12:47:30 -0500

    creacion de servicio para firestore

---
+++ src/app/dashboard/services/firestore.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(private firestore: AngularFirestore) { }
+}
