5c327b2624a99bb0c74fed8b351a5ee04034894c
sandrahfiestas <shfiestas@gmail.com>
2020-11-25 23:27:59 -0500

    generando word con html-docx y file_saver

--- src/app/dashboard/contract/contract.module.ts
+++ src/app/dashboard/contract/contract.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ContractRoutingModule } from './contract-routing.module';
 import { CreateContractComponent } from './create-contract/create-contract.component';
 import { ContractStatesComponent } from './contract-states/contract-states.component';
+import { ContractDownloadComponent } from './contract-download/contract-download.component';
 
 
 @NgModule({
-  declarations: [CreateContractComponent, ContractStatesComponent],
+  declarations: [CreateContractComponent, ContractStatesComponent, ContractDownloadComponent],
   imports: [
     CommonModule,
     ContractRoutingModule
