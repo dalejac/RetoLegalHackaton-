32b694cc0c3bcc01947320d002b2d10ff216cba7
Mayra <mayrarodriguez1709@gmail.com>
2020-11-27 15:20:34 -0500

    fix: uppercase validation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAzCSLNAfWZojks2WJGGzkQ-0uJmKvjihI',
+    authDomain: 'reto-porvenir.firebaseapp.com',
+    databaseURL: 'https://reto-porvenir.firebaseio.com',
+    projectId: 'reto-porvenir',
+    storageBucket: 'reto-porvenir.appspot.com',
+    messagingSenderId: '6086545622',
+  }
 };
