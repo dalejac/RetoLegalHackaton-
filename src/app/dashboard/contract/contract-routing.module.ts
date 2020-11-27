a857023762868ae293a90a3e24a60913ff1bb720
dalejac <k-da929@hotmail.com>
2020-11-26 22:44:42 -0500

    Commit

--- src/app/dashboard/contract/contract-routing.module.ts
+++ src/app/dashboard/contract/contract-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CreateContractComponent} from './create-contract/create-contract.component';
 import {ContractStatesComponent} from './contract-states/contract-states.component';
-// import { ContractDownloadComponent } from './contract-download/contract-download.component';
 
 
 const routes: Routes = [
@@ -14,10 +13,6 @@ const routes: Routes = [
     path: 'states',
     component: ContractStatesComponent
   },
-  // {
-  //   path: 'prueba',
-  //   component: ContractDownloadComponent
-  // },
   {
     path: '**',
     redirectTo: 'create'
--- src/app/dashboard/contract/contract.module.ts
+++ src/app/dashboard/contract/contract.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ContractRoutingModule } from './contract-routing.module';
 import { CreateContractComponent } from './create-contract/create-contract.component';
 import { ContractStatesComponent } from './contract-states/contract-states.component';
-// import { ContractDownloadComponent } from './contract-download/contract-download.component';
 import {SharedModule} from '../../shared/shared.module';
-import { DownloadContractComponent } from './download-contract/download-contract.component';
+
 
 
 @NgModule({
-  // declarations: [CreateContractComponent, ContractStatesComponent, ContractDownloadComponent, DownloadContractComponent],
-  declarations: [CreateContractComponent, ContractStatesComponent, DownloadContractComponent],
+  declarations: [CreateContractComponent, ContractStatesComponent],
 
   imports: [
     CommonModule,
